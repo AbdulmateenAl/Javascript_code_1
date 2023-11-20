75ede8411a8699f40cc8634cae6bfd80232e6146
Abdulmateen <alabiabdulmateen94@gmail.com>
2023-11-20 09:48:14 +0100

    prototypes and inheritance added

--- test.js
+++ test.js
@@ -5,3 +5,19 @@ function multiply(num1, num2) {
     return result
 }
 multiply(5,5);
+
+// Dealing with Prototypes
+const personPrototype = {
+    greet() {
+        console.log(`Hello ${this.name}`);
+    },
+};
+
+function Person(name) {
+    this.name = name;
+}
+
+Object.assign(Person.prototype, personPrototype);
+
+const abdul = new Person("Abdul");
+abdul.greet()
